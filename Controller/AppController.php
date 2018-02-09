25b82cb3202da2e73d28dbc157986a3d318059b7
sana1102 <masatoshi_key_of_angel@yahoo.co.jp>
2018-02-09 22:00:25 +0900

    login0209-1

---
+++ Controller/AppController.php
@@ -0,0 +1,30 @@
+<?php
+
+App::uses('Controller', 'Controller');
+
+class AppController extends Controller {
+
+	public $components = array(
+        'Flash',
+        'Auth' => array(
+            'loginRedirect' => array(
+                'controller' => 'posts',
+                'action' => 'index'
+            ),
+            'logoutRedirect' => array(
+                'controller' => 'pages',
+                'action' => 'display',
+                'home'
+            ),
+            'authenticate' => array(
+                'Form' => array(
+                    'passwordHasher' => 'Blowfish'
+                )
+            )
+        )
+    );
+
+    public function beforeFilter() {
+        $this->Auth->allow('index', 'view');
+    }
+}
